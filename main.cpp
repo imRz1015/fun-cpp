d66703c6b5e5dca4aad99ebb1b0bc14095c4728f
汤启民 <tangiming@foxmail.com>
2022-01-13 18:04:16 +0800

    feat: 初始化项目

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: 汤启民
+ * @Date: 2022-01-13 16:14:42
+ * @Description: main
+ */
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "first c++" << endl;
+  return 0;
+}
